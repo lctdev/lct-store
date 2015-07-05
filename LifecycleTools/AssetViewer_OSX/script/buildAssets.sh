9818f5704c96eb2cb74f5d3d494fcbc2c254f404
lctdev <lctdev01@gmail.com>
2015-07-05 15:45:57 -0700

    xcode fixup
    updating the Xcode files for the new asset structure

---
+++ LifecycleTools/AssetViewer_OSX/script/buildAssets.sh
@@ -0,0 +1,9 @@
+ASSET_COMPILER_PATH=../AssetViewer/AssetCompiler/AssetCompiler.jar
+
+MANIFEST_PATH=script/programAssets.xml
+TEMP_DIR_PATH=../temp
+PACKAGE_PATH=$TARGET_BUILD_DIR/data/programAssets.bin
+
+echo $PACKAGE_PATH
+
+java -jar $ASSET_COMPILER_PATH -manifest=$MANIFEST_PATH -temp=$TEMP_DIR_PATH -package=$PACKAGE_PATH
